deed1534d07c70936b7fd45cecd03eb650984eea
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-05-28 18:49:14 +0430

    feat(app-routing.module): add routes for manage-reports and empty and ** wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent} from "../../shared-components/src/app/main-layout/main-layout.component";
+import { MainLayoutModule} from "../../shared-components/src/app/main-layout/main-layout.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'manage-reports', pathMatch: 'full'},
+  {path: 'manage-reports', component:MainLayoutComponent, loadChildren:() => import('./manage-reports/manage-reports.module').then(m => m.ManageReportsModule)},
+  {path: '**', redirectTo: 'manage-reports'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    MainLayoutModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
