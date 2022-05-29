649679d1d4a385caa0794828566dcf721e867221
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-05-29 17:40:10 +0430

    feat(app.module.ts): import MainLayout Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MainLayoutModule} from "../../shared-components/src/app/main-layout/main-layout.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MainLayoutModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
