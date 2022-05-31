de196023e223d9ef8ed923b053f626511df8c005
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-05-31 18:42:04 +0430

    feat(app.component.ts): inject layoutService and read headerConfig actionbarConfig from it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {MainLayoutService} from "./services/main-layout.service";
+import {HeaderModel} from "../../shared-components/src/app/main-layout/models/header.model";
+import {Subscription} from "rxjs";
+import {ActionbarModel} from "../../shared-components/src/app/main-layout/models/actionbar.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   title = 'survey-front';
+  headerConfig: HeaderModel;
+  actionbarConfig: ActionbarModel;
+
+  subscriptions = new Subscription();
+
+  constructor(private layoutService: MainLayoutService) {
+    this.subscriptions.add(
+      this.layoutService.headerConfigSubject.subscribe(header => {
+        this.headerConfig = header;
+      })
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
