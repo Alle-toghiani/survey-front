6c778fc29b1ffb7c6c7ef4fa6bf514657fccb920
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-05-31 18:44:19 +0430

    feat(reports-list): set headerConfig

--- src/app/manage-reports/components/reports-list/reports-list.component.ts
+++ src/app/manage-reports/components/reports-list/reports-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MainLayoutService} from "../../../services/main-layout.service";
+import {HeaderModel} from "../../../../../shared-components/src/app/main-layout/models/header.model";
 
 @Component({
   selector: 'app-reports-list',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reports-list.component.scss']
 })
 export class ReportsListComponent implements OnInit {
-
-  constructor() { }
+  headerConfig: HeaderModel = {
+    title : {value: 'مجموعه شماره یک'},
+    subtitle: {value: 'نظرسنجی کارکنان'},
+  }
+  constructor(
+    private layoutService: MainLayoutService
+  ) { }
 
   ngOnInit(): void {
+    this.layoutService.headerConfigSubject.next(this.headerConfig);
   }
 
 }
