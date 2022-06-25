383a1a98d48e7e8139a1486f875c59162488f1ff
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-06-25 17:13:40 +0430

    feat(environment.ts): add porslineBaseUrl and backendBaseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  porslineBaseUrl : 'https://survey.porsline.ir/api/',
+  backendBaseUrl: 'http://localhost:3000/'
 };
 
 /*
