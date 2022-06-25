e80c9247fa241b78a1b4f4b0434070b763c0d8c1
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-06-25 17:14:49 +0430

    feat(app/models/survey.model.ts): add SurveyModel & its required types

---
+++ src/app/models/survey.model.ts
@@ -0,0 +1,59 @@
+export interface SurveyModel {
+  id: number;
+  data: {[key:string|number] : SurveyQuestion};
+  name: string;
+  language: number;
+  active: boolean;
+  can_active: boolean;
+  massage_text?: string;
+  views: number;
+  submitted_responses: number;
+  closed: boolean;
+  is_stopped: boolean;
+  is_not_started: boolean;
+  deleted: boolean;
+  preview_code: string;
+  report_code: string;
+  score_charts_active: boolean;
+  theme: number;
+  background_image_config?: string;
+  created_date: string;
+  parent?: string;
+  folder: {
+    id: number,
+    order: number,
+    name: string
+  }
+}
+
+interface SurveyQuestion{
+  choices: SurveyChoice[];
+  charts: SurveyCharts;
+  id: number;
+  title: string;
+  type: number;
+  order: number;
+  show_charts: boolean;
+  image_video_active: boolean;
+  image_or_video: number;
+  image_path: string;
+  video_url: string;
+  image_name: string;
+  question_number_is_hidden: boolean;
+  related_group: string;
+  allow_multiple_select: boolean;
+}
+
+interface SurveyCharts{
+  id: number;
+  title: string;
+  type: number | string;
+  order: number;
+  caption: string;
+}
+
+export class SurveyChoice{
+  id: number;
+  title: string;
+  frequency: number;
+}
