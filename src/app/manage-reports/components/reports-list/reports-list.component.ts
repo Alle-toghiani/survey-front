015d4d867591d82157ac6d2ad0b3cfb0b9fea4d4
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-06-25 17:16:43 +0430

    feat(reports-list.ts): read id from route, get survey with that id

--- src/app/manage-reports/components/reports-list/reports-list.component.ts
+++ src/app/manage-reports/components/reports-list/reports-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {MainLayoutService} from "../../../services/main-layout.service";
 import {HeaderModel} from "../../../../../shared-components/src/app/main-layout/models/header.model";
+import {Router, ActivatedRoute, ParamMap} from "@angular/router";
+import {SurveyModel} from "../../../models/survey.model";
+import {Subscription} from "rxjs";
+import {ManageReportsHttpService} from "../../services/manage-reports-http.service";
 
 @Component({
   selector: 'app-reports-list',
@@ -12,12 +16,45 @@ export class ReportsListComponent implements OnInit {
     title : {value: 'مجموعه شماره یک'},
     subtitle: {value: 'نظرسنجی کارکنان'},
   }
+  surveyId: string;
+  survey: SurveyModel;
+  subscriptions = new Subscription();
+
   constructor(
-    private layoutService: MainLayoutService
-  ) { }
+    private layoutService: MainLayoutService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private httpService: ManageReportsHttpService
+  ) {}
 
   ngOnInit(): void {
-    this.layoutService.headerConfigSubject.next(this.headerConfig);
+    this.route.params.subscribe( params => {
+      this.surveyId = params['id'];
+      this.isRouteValid(this.surveyId) ? this.getSurveyInfo(+this.surveyId) : this.redirectToNotFound();
+    })
+  }
+
+  isRouteValid(id: string): boolean{
+    return (!isNaN(+id) && id.length === 6)
+  }
+
+  redirectToNotFound():void{
+    this.router.navigate(['/not-found'], {relativeTo: null});
+  }
+
+  getSurveyInfo(surveyId: number): void{
+    this.subscriptions.add(
+      this.httpService.getSurvey(surveyId).subscribe(
+        res => {
+          this.survey = res;
+          this.headerConfig.subtitle.value = this.survey.name;
+          this.layoutService.headerConfigSubject.next(this.headerConfig);
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    )
   }
 
 }
