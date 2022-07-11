927ee0029196368ba8b16877858318d71c36f915
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-07-11 19:31:29 +0430

    feat(app.module.ts): initialize TranslateModule (ngx-translate)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from "@angular/common/http";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 import { AngularSvgIconModule} from "angular-svg-icon";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MainLayoutModule} from "../../shared-components/src/app/main-layout/main-layout.module";
+
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -17,7 +25,15 @@ import {MainLayoutModule} from "../../shared-components/src/app/main-layout/main
       AppRoutingModule,
       MainLayoutModule,
       HttpClientModule,
-      AngularSvgIconModule.forRoot()
+      AngularSvgIconModule.forRoot(),
+      TranslateModule.forRoot({
+        defaultLanguage: 'fa',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        }
+      })
     ],
   providers: [],
   bootstrap: [AppComponent]
