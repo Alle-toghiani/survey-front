31a40df54fbeb55dd089e463b90651eb7fc85574
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-07-15 21:41:58 +0430

    feat(manage-reports-routing.module.ts): add route for SurveyDetailsComponent

--- src/app/manage-reports/manage-reports-routing.module.ts
+++ src/app/manage-reports/manage-reports-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReportsListComponent} from "./components/reports-list/reports-list.component";
 import { RoutesEnum} from "../enums/routes.enum";
+import { SurveyDetailsComponent} from "./components/survey-details/survey-details.component";
 
 const routes: Routes = [
-  {path: ':' + RoutesEnum.SURVEY_ID_PARAM, component: ReportsListComponent }
+  {path: ':' + RoutesEnum.SURVEY_ID_PARAM, component: ReportsListComponent },
+  {path: ':' + RoutesEnum.SURVEY_ID_PARAM + '/details/:' + RoutesEnum.QUESTION_ID_PARAM, component: SurveyDetailsComponent}
 ];
 
 @NgModule({
