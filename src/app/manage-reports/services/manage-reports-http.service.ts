7ed18385e0b0d8d9c0a9c258ec6f73b9e2df769c
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-07-17 13:41:25 +0430

    refactor(manage-reports-http.service): import and implement SharedModel, create getSurveyQuestionDetails

--- src/app/manage-reports/services/manage-reports-http.service.ts
+++ src/app/manage-reports/services/manage-reports-http.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from "@angular/common/http";
+import { Observable, from} from "rxjs";
+
 import { environment} from "../../../environments/environment";
-import { SurveyModel} from "../../models/survey.model";
-import {Observable, from} from "rxjs";
+import { SharedModel} from "../../models/shared.model";
+import { SurveyModel, SurveyQuestion} from "../../models/survey.model";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,11 @@ export class ManageReportsHttpService {
 
   constructor(private http: HttpClient) { }
 
-  getSurvey(surveyId: number): Observable<SurveyModel>{
-    return this.http.get<SurveyModel>(environment.backendBaseUrl + 'survey/' + surveyId);
+  getSurvey(surveyId: number): Observable<SharedModel<SurveyModel>>{
+    return this.http.get<SharedModel<SurveyModel>>(environment.backendBaseUrl + 'survey/' + surveyId);
+  }
+
+  getSurveyQuestionDetails(sid: string, qid: string): Observable<SharedModel<SurveyQuestion>>{
+    return this.http.get<SharedModel<SurveyQuestion>>(environment.backendBaseUrl + 'survey/' + sid + '/details/' + qid)
   }
 }
