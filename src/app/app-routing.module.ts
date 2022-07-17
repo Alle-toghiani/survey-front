d6544fd60f4709015de9306ecea552acb3270605
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-07-17 13:35:54 +0430

    feat(app-routing.module.ts): add route for not-found-page, dashboard, import RoutesEnum and use it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { RoutesEnum} from "./enums/routes.enum";
 import { MainLayoutModule} from "@shared-components/src/app/main-layout/main-layout.module";
+import { NotFoundPageComponent} from "./general-pages/not-found-page/not-found-page.component";
 
 const routes: Routes = [
   {path: '', redirectTo:'manage-reports', pathMatch: 'full'},
-  {path: 'manage-reports', loadChildren:() => import('./manage-reports/manage-reports.module').then(m => m.ManageReportsModule)},
+  {path: RoutesEnum.DASHBOARD, redirectTo: 'manage-reports'},
+  {path: RoutesEnum.SURVEYS, loadChildren:() => import('./manage-reports/manage-reports.module').then(m => m.ManageReportsModule)},
+  {path: RoutesEnum.NOT_FOUND_PAGE, loadComponent: () => import('./general-pages/not-found-page/not-found-page.component').then((m) => m.NotFoundPageComponent)},
   {path: '**', redirectTo: 'manage-reports'}
 ];
 
