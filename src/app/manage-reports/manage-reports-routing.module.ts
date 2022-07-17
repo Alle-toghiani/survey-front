adb221ff56767447d8052f9e9e62900c577a2d62
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-07-17 13:43:56 +0430

    feat(manage-reports-routing.module): add resolver to SurveyResolver to both paths

--- src/app/manage-reports/manage-reports-routing.module.ts
+++ src/app/manage-reports/manage-reports-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ReportsListComponent} from "./components/reports-list/reports-list.component";
+
 import { RoutesEnum} from "../enums/routes.enum";
+import { SurveyResolver} from "./services/survey.resolver";
+import { ReportsListComponent} from "./components/reports-list/reports-list.component";
 import { SurveyDetailsComponent} from "./components/survey-details/survey-details.component";
 
 const routes: Routes = [
-  {path: ':' + RoutesEnum.SURVEY_ID_PARAM, component: ReportsListComponent },
-  {path: ':' + RoutesEnum.SURVEY_ID_PARAM + '/details/:' + RoutesEnum.QUESTION_ID_PARAM, component: SurveyDetailsComponent}
+  { path: ':' + RoutesEnum.SURVEY_ID_PARAM,
+    component: ReportsListComponent,
+    resolve: { surveyData: SurveyResolver} },
+
+  { path: ':' + RoutesEnum.SURVEY_ID_PARAM + '/details/:' + RoutesEnum.QUESTION_ID_PARAM,
+    component: SurveyDetailsComponent,
+    resolve: { surveyData: SurveyResolver}}
 ];
 
 @NgModule({
