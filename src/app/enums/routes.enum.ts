ef259646c54f711dace335f0b69a6cc52792e8c6
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-07-17 13:34:15 +0430

    feat(routes.enum.ts): add SURVEYS, NOT_FOUND_PAGE, DASHBOARD

--- src/app/enums/routes.enum.ts
+++ src/app/enums/routes.enum.ts
@@ -1,4 +1,8 @@
 export enum RoutesEnum {
+  SURVEYS = 'surveys',
   SURVEY_ID_PARAM = 'sid',
-  QUESTION_ID_PARAM = 'qid'
+  QUESTION_ID_PARAM = 'qid',
+
+  NOT_FOUND_PAGE = 'not-found',
+  DASHBOARD = 'dashboard'
 }
