6bc5d29cd72a458c00a3b9723fd9a7a555524b6d
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-07-18 17:43:34 +0430

    refactor(manage-reports-routing.module): lazy load SurveyDetails module, remove SurveyDetailsComp import

--- src/app/manage-reports/manage-reports-routing.module.ts
+++ src/app/manage-reports/manage-reports-routing.module.ts
@@ -4,16 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { RoutesEnum} from "../enums/routes.enum";
 import { SurveyResolver} from "./services/survey.resolver";
 import { ReportsListComponent} from "./components/reports-list/reports-list.component";
-import { SurveyDetailsComponent} from "./components/survey-details/survey-details.component";
 
 const routes: Routes = [
-  { path: ':' + RoutesEnum.SURVEY_ID_PARAM,
+  {
+    path: ':' + RoutesEnum.SURVEY_ID_PARAM,
     component: ReportsListComponent,
-    resolve: { surveyResolverData: SurveyResolver} },
+    resolve: { surveyResolverData: SurveyResolver}
+  },
 
-  { path: ':' + RoutesEnum.SURVEY_ID_PARAM + '/details/:' + RoutesEnum.QUESTION_ID_PARAM,
-    component: SurveyDetailsComponent,
-    resolve: { surveyResolverData: SurveyResolver}}
+  {
+    path: ':' + RoutesEnum.SURVEY_ID_PARAM + '/details/:' + RoutesEnum.QUESTION_ID_PARAM,
+    loadChildren: () => import('./features/survey-details/survey-details.module').then(m => m.SurveyDetailsModule),
+    resolve: { surveyResolverData: SurveyResolver}
+  }
 ];
 
 @NgModule({
