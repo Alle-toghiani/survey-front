75b01e4f8ef5fed53bb3984f70820598c4f129a1
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-07-25 20:39:03 +0430

    feat(survey-details.comp.ts): load selectedChartType

--- src/app/manage-reports/features/survey-details/survey-details.component.ts
+++ src/app/manage-reports/features/survey-details/survey-details.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {SurveyModel, SurveyQuestion} from "../../../models/survey.model";
-import {SharedModel} from "../../../models/shared.model";
+import { ActivatedRoute} from "@angular/router";
 
-enum ChartType {
-  BAR = "bar",
-  PIE = "pie",
-  DOUGHNUT = "doughnut",
-  LINE = "line",
-  POLAR_AREA = "polarArea",
-  RADAR = "RADAR"
+import { SurveyModel, SurveyQuestion} from "../../../models/survey.model";
+import { SharedModel} from "../../../models/shared.model";
+
+const ChartTypes = {
+  1: "bar",
+  2: "pie",
+  BAR : "bar",
+  PIE : "pie",
+  DOUGHNUT : "doughnut",
+  LINE : "line",
+  POLAR_AREA : "polarArea",
+  RADAR : "radar",
 }
+
+type ChartTypes = typeof ChartTypes[keyof typeof ChartTypes];
 
 @Component({
   selector: 'app-survey-details',
@@ -20,13 +25,28 @@ enum ChartType {
 export class SurveyDetailsComponent implements OnInit {
 
   questionData: SurveyQuestion;
+
+  selectedChartType: ChartTypes;
+  chartInvalidError = false;
   constructor(
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.questionData = (this.route.snapshot.data['surveyResolverData'] as SharedModel<SurveyQuestion>).data;
+    this.loadDataInit();
   }
 
+  loadDataInit(): void{
+    this.questionData = (this.route.snapshot.data['surveyResolverData'] as SharedModel<SurveyQuestion>).data;
+    const chartType = this.questionData.charts[0].type;
+    if (chartType && ChartTypes[chartType]) {
+      this.selectedChartType = ChartTypes[chartType]
+    } else {
+      this.chartInvalidError = true;
+    }
+  }
 
+  get chartTypesValues(): string[]{
+    return [... new Set(Object.values(ChartTypes))]
+  }
 }
