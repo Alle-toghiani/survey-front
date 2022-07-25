087522e1c41da4ee6277fb8471671c9ce4a48a58
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-07-25 20:36:43 +0430

    feat(survey-details.module): import FormsModule, NzModules and AngularSvgIconModule

--- src/app/manage-reports/features/survey-details/survey-details.module.ts
+++ src/app/manage-reports/features/survey-details/survey-details.module.ts
@@ -7,6 +7,15 @@ import { TranslateModule} from "@ngx-translate/core";
 import { SurveyDetailsComponent} from "./survey-details.component";
 import { WidgetContainerModule} from "@shared-components/src/app/widget-container/widget-container.module";
 
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { FormsModule} from "@angular/forms";
+import { AngularSvgIconModule} from "angular-svg-icon";
+
 const routes: Routes = [
   {
     path: '',
@@ -22,7 +31,15 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes),
     TranslateModule,
-    WidgetContainerModule
+    WidgetContainerModule,
+    NzButtonModule,
+    NzDropDownModule,
+    NzToolTipModule,
+    NzIconModule,
+    NzInputModule,
+    NzSelectModule,
+    FormsModule,
+    AngularSvgIconModule
   ]
 })
 export class SurveyDetailsModule { }
