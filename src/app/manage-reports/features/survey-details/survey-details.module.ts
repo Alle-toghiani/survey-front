30a9406c9b9cd06766aef889e255469251bdb007
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-05 16:25:49 +0430

    feat(survey-details.module): import NzDividerModule

--- src/app/manage-reports/features/survey-details/survey-details.module.ts
+++ src/app/manage-reports/features/survey-details/survey-details.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes} from "@angular/router";
+import { FormsModule} from "@angular/forms";
 
 import { TranslateModule} from "@ngx-translate/core";
-
-import { SurveyDetailsComponent} from "./survey-details.component";
-import { WidgetContainerModule} from "@shared-components/src/app/widget-container/widget-container.module";
-
+import { AngularSvgIconModule} from "angular-svg-icon";
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzSelectModule } from 'ng-zorro-antd/select';
-import { FormsModule} from "@angular/forms";
-import { AngularSvgIconModule} from "angular-svg-icon";
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+
+import { SurveyDetailsComponent} from "./survey-details.component";
+import { WidgetContainerModule} from "@shared-components/src/app/widget-container/widget-container.module";
 
 const routes: Routes = [
   {
@@ -30,7 +30,9 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    FormsModule,
     TranslateModule,
+    AngularSvgIconModule,
     WidgetContainerModule,
     NzButtonModule,
     NzDropDownModule,
@@ -38,8 +40,7 @@ const routes: Routes = [
     NzIconModule,
     NzInputModule,
     NzSelectModule,
-    FormsModule,
-    AngularSvgIconModule
+    NzDividerModule,
   ]
 })
 export class SurveyDetailsModule { }
