f5dad946dd7ada6ccc4d848ab113f3bb4713e800
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-08 20:54:20 +0430

    feat(results.module): create result-item.component

--- src/app/features/results/results.module.ts
+++ src/app/features/results/results.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ResultsRoutingModule } from './results-routing.module';
 import { ResultsContainerComponent } from './components/results-container/results-container.component';
+import { ResultItemComponent } from './components/result-item/result-item.component';
 
 
 @NgModule({
   declarations: [
-    ResultsContainerComponent
+    ResultsContainerComponent,
+    ResultItemComponent
   ],
   imports: [
     CommonModule,
