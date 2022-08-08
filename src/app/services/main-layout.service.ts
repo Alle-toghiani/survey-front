017068c7ff878c97f2f6e509e0620d3c94985542
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-08 17:45:04 +0430

    feat/refactor(main-layout.service): change imports order, add actionbarConfigSubejct prop, actionbarClickEvent event

--- src/app/services/main-layout.service.ts
+++ src/app/services/main-layout.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
-import {HeaderModel} from "../../../shared-components/src/app/main-layout/models/header.model";
-import {Subject, Subscription} from "rxjs";
+import {EventEmitter, Injectable} from '@angular/core';
+import { Subject } from "rxjs";
+
+import { HeaderModel } from "@shared-components/src/app/main-layout/models/header.model";
+import { ActionbarModel } from "@shared-components/src/app/main-layout/models/actionbar.model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +11,11 @@ export class MainLayoutService {
 
   headerConfigSubject = new Subject<HeaderModel>();
 
+  actionbarConfigSubject = new Subject<ActionbarModel>();
+
+  actionbarClickEvent = new EventEmitter<string>();
 
   constructor() {
-
   }
 
 }
