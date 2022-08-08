0bde508d922d7e0dbbedb3954dd8fd5ecd6a513c
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-08 17:36:12 +0430

    feat(manage-reports-http.service): add updateChartSettings method, import CustomChartSettings

--- src/app/manage-reports/services/manage-reports-http.service.ts
+++ src/app/manage-reports/services/manage-reports-http.service.ts
@@ -4,7 +4,7 @@ import { Observable, from} from "rxjs";
 
 import { environment} from "../../../environments/environment";
 import { SharedModel} from "../../models/shared.model";
-import { SurveyModel, SurveyQuestion} from "../../models/survey.model";
+import { SurveyModel, SurveyQuestion, CustomChartSettings} from "../../models/survey.model";
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +19,10 @@ export class ManageReportsHttpService {
 
   getSurveyQuestionDetails(sid: string, qid: string): Observable<SharedModel<SurveyQuestion>>{
     return this.http.get<SharedModel<SurveyQuestion>>(environment.backendBaseUrl + 'survey/' + sid + '/details/' + qid)
+  }
+
+  updateChartSettings(sid: string, qid: string, data: CustomChartSettings): Observable<SharedModel<any>>{
+    const url = environment.backendBaseUrl + 'survey/' + sid + '/details/' + qid
+    return this.http.put<SharedModel<any>>(url, data);
   }
 }
