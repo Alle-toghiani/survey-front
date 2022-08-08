b2d900450bd1d6e89002966c09145ad91640150f
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-08 17:46:48 +0430

    feat(survey-details.comp.ts): add actionbar btn logic, chartSettingsUpdate logic

--- src/app/manage-reports/features/survey-details/survey-details.component.ts
+++ src/app/manage-reports/features/survey-details/survey-details.component.ts
@@ -1,11 +1,18 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute} from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 
 import Chart from 'chart.js/auto';
 import { ChartTypeRegistry } from "chart.js";
+import { Subscription} from "rxjs";
+import { TranslateService} from "@ngx-translate/core";
 
+import { ActionbarModel, ActionItem } from "@shared-components/src/app/main-layout/models/actionbar.model";
+import { ManageReportsHttpService } from "../../services/manage-reports-http.service";
+import { MainLayoutService } from "../../../services/main-layout.service";
 import { SurveyQuestion } from "../../../models/survey.model";
 import { SharedModel } from "../../../models/shared.model";
+import { RoutesEnum } from "../../../enums/routes.enum";
+
 
 const ChartTypes = {
   1: "bar",
@@ -40,29 +47,59 @@ const colors = [
 export class SurveyDetailsComponent implements OnInit, AfterViewInit {
 
   @ViewChild('canvas') canvasRef: ElementRef;
+  surveyId: string;
   ctx;
   chart;
   questionData: SurveyQuestion;
   isLoading = true;
   selectedChartType: ChartTypes;
   chartInvalidError = false;
+  subscriptions = new Subscription();
+  actionbarItems: ActionbarModel;
+  actionbarSaveChanges: ActionItem;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private layoutService: MainLayoutService,
+    private translateService: TranslateService,
+    private httpService: ManageReportsHttpService
   ) { }
 
   ngOnInit(): void {
+    this.surveyId = this.route.snapshot.params[RoutesEnum.SURVEY_ID_PARAM];
     this.questionData = (this.route.snapshot.data['surveyResolverData'] as SharedModel<SurveyQuestion>).data;
+
+    this.layoutService.actionbarClickEvent.subscribe(item => {
+      this.onActionbarClicked(item);
+    })
   }
 
   ngAfterViewInit() {
     if (this.questionData){
       this.initChart();
     }
+    this.initActionbar();
+  }
+
+  initActionbar(): void{
+    this.actionbarSaveChanges = {
+      title: this.translator('survey-details.save-changes'),
+      iconType: 'save',
+      color: "green"
+    }
+    this.updateActionBar();
+  }
+
+  updateActionBar():void{
+    this.layoutService.actionbarConfigSubject.next(
+      {
+        actions: [this.actionbarSaveChanges]
+      }
+    );
   }
 
   initChart(): void{
-    const chartType = this.questionData.charts[0].type;
+    const chartType = this.questionData.customChartSettings ? this.questionData.customChartSettings.type : this.questionData.charts[0].type;
     if (chartType && ChartTypes[chartType]) {
       this.selectedChartType = ChartTypes[chartType];
       this.generateChartData();
@@ -117,7 +154,55 @@ export class SurveyDetailsComponent implements OnInit, AfterViewInit {
     }
   }
 
+  onActionbarClicked(itemTitle: string){
+    switch (itemTitle) {
+      case this.translator('survey-details.save-changes'): {
+        this.saveChartChanges();
+      }break;
+    }
+  }
+
+  saveChartChanges(): void {
+    if (this.isChartSettingsChanged()){
+      const chartUpdateData = {
+        type: this.selectedChartType
+      }
+      this._setLoading(true);
+      this.subscriptions.add(
+        this.httpService.updateChartSettings(this.surveyId, this.questionData.id.toString(), chartUpdateData).subscribe(
+          {
+            next: updateRes => {
+              this._setLoading(false);
+              this.questionData.customChartSettings = chartUpdateData;
+            },
+            error: err => this._setLoading(false)
+          }
+        )
+      )
+    }
+
+  }
+
+  isChartSettingsChanged(): boolean{
+    return !this.questionData.customChartSettings || this.questionData.customChartSettings.type !== this.selectedChartType;
+  }
+
   get chartTypesValues(): string[]{
     return [... new Set(Object.values(ChartTypes))]
+  }
+
+  translator(word: string): string{
+    let translatedWord = "";
+    this.subscriptions.add(this.translateService.get(word.toLowerCase()).subscribe(
+      result => {
+        translatedWord = result;
+      }
+    ));
+    return translatedWord;
+  }
+
+  private _setLoading(isLoading: boolean): void{
+    this.actionbarSaveChanges.isLoading = isLoading;
+    this.updateActionBar();
   }
 }
