b106832e4fe9ffaf5513f5d3f4a8e61de8b87cc9
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-12 14:23:40 +0430

    feat(app/services/index.ts): export SurveyResolver

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,2 +1,3 @@
 export { MainLayoutService } from './main-layout.service';
 export { CustomValidatorsService } from './custom-validators.service';
+export { SurveyResolver } from './survey.resolver';
