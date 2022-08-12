e16f93fca452fd7d0d9d418aefe7768dcfd8de35
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-12 14:55:44 +0430

    feat(services/index.ts): export ResponseNotificationInterceptor

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,3 +1,4 @@
 export { MainLayoutService } from './main-layout.service';
 export { CustomValidatorsService } from './custom-validators.service';
 export { SurveyResolver } from './survey.resolver';
+export { ResponseNotificationInterceptor } from './http-interceptors/response-notification.interceptor';
