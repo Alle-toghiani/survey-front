634cfa3056f3aacf0f97b3075afe1bef4e8bf7a8
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-12 14:23:16 +0430

    refactor(manage-reports-routing.module): shorten RoutesEnum and SurveyResolver import paths

--- src/app/manage-reports/manage-reports-routing.module.ts
+++ src/app/manage-reports/manage-reports-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RoutesEnum} from "../enums/routes.enum";
-import { SurveyResolver} from "./services/survey.resolver";
-import { ReportsListComponent} from "./components/reports-list/reports-list.component";
+import { RoutesEnum } from "@enums";
+import { SurveyResolver } from "@services";
+import { ReportsListComponent } from "./components/reports-list/reports-list.component";
 
 const routes: Routes = [
   {
@@ -11,7 +11,6 @@ const routes: Routes = [
     component: ReportsListComponent,
     resolve: { surveyResolverData: SurveyResolver}
   },
-
   {
     path: ':' + RoutesEnum.SURVEY_ID_PARAM + '/details/:' + RoutesEnum.QUESTION_ID_PARAM,
     loadChildren: () => import('./features/survey-details/survey-details.module').then(m => m.SurveyDetailsModule),
