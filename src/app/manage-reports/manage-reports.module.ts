9606759b6c9800a5ae4c531c38b729849eb958c2
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-12 15:38:15 +0430

    refactor(manage-reports.module): fix order of imports

--- src/app/manage-reports/manage-reports.module.ts
+++ src/app/manage-reports/manage-reports.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ManageReportsRoutingModule } from './manage-reports-routing.module';
-import { ReportsListComponent } from './components/reports-list/reports-list.component';
-import { WidgetContainerModule} from "@shared-components/src/app/widget-container/widget-container.module";
-import { TablesModule} from "@shared-components/src/app/tables/tables.module";
 import { TranslateModule} from "@ngx-translate/core";
 import { NzButtonModule } from 'ng-zorro-antd/button';
+
+import { WidgetContainerModule} from "@shared-components/src/app/widget-container/widget-container.module";
+import { TablesModule} from "@shared-components/src/app/tables/tables.module";
+import { ManageReportsRoutingModule } from './manage-reports-routing.module';
+import { ReportsListComponent } from './components/reports-list/reports-list.component';
 
 @NgModule({
   declarations: [
