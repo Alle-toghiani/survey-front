75f9f738dc79a3abed44b33eff2d189598f98e05
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-16 15:36:20 +0430

    refactor(not-found-page.comp.ts): fix import paths and order

--- src/app/common/not-found-page/not-found-page.component.ts
+++ src/app/common/not-found-page/not-found-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NzButtonModule } from 'ng-zorro-antd/button';
+import { Router } from "@angular/router";
+
 import { TranslateModule} from "@ngx-translate/core";
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { AccountBookFill, AlertFill, AlertOutline } from '@ant-design/icons-angular/icons';
-import {Router} from "@angular/router";
-import {RoutesEnum} from "../../enums/routes.enum";
+
+import { RoutesEnum } from "@enums";
 
 @Component({
   selector: 'app-not-found-page',
