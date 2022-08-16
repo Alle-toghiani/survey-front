116e7ed456e7e9ad3bc1e2a71271321c00454c66
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-16 17:10:13 +0430

    feat(dashboard.module): declare dashboardComponent, import WidgetCOntainer and TranslateModule

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TranslateModule } from "@ngx-translate/core";
+
+import { WidgetContainerModule } from "@shared-components/src/app/widget-container/widget-container.module";
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { FoldersSurveysListComponent } from './components/folders-surveys-list/folders-surveys-list.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
+    DashboardComponent,
     FoldersSurveysListComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    WidgetContainerModule,
+    TranslateModule
   ]
 })
 export class DashboardModule { }
