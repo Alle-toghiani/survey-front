0b462f7d839a33aeda7f3577a3113dc7e28fd3b7
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-17 15:13:44 +0430

    feat(dashboard/components): create dashboard component

---
+++ src/app/features/dashboard/components/dashboard/dashboard.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from "rxjs";
+
+import { DashboardHttpService } from "../../services/dashboard-http.service";
+import { FolderModel } from "@models";
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit, OnDestroy {
+
+  subscriptions = new Subscription();
+  folders: FolderModel[] = [];
+
+  constructor(
+    private dashboardService: DashboardHttpService
+  ) { }
+
+  ngOnInit(): void {
+    this.getFoldersData();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  getFoldersData(): void {
+    this.subscriptions.add(
+      this.dashboardService.getMockFolders().subscribe({
+        next: (response) => {
+          console.log(response)
+          this.folders = response;
+          // if (response.success){
+          //   this.folders = response.data;
+          // }
+        }
+      })
+    )
+  }
+}
