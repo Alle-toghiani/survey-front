0ff7957d02984857dda92a273de7aaa4e4bc3fbf
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-17 19:49:07 +0430

    feat(auth/signup): create signpu template

--- src/app/features/auth/components/signup/signup.component.ts
+++ src/app/features/auth/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  validateForm = new FormGroup({
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+    remember: new FormControl(true)
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submitForm(): void {
+    console.log('submit', this.validateForm.value);
   }
 
 }
