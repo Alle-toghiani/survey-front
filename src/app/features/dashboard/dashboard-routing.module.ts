553b370c14364b20227080d8ae63c5ecb33c2cef
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-17 18:52:11 +0430

    feat(dashboard-routing.module): lazy load surveys as a child route, lazy load not-found

--- src/app/features/dashboard/dashboard-routing.module.ts
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -2,9 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {RoutesEnum} from "@enums";
+import {FoldersSurveysListComponent} from "./components/folders-surveys-list/folders-surveys-list.component";
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent }
+  {
+    path: '', component: DashboardComponent, children: [
+      {
+        path: '', component: FoldersSurveysListComponent
+      },
+      {
+        path: RoutesEnum.SURVEYS,
+        loadChildren:() => import('../manage-reports/manage-reports.module').then(m => m.ManageReportsModule)
+      },
+      {
+        path: RoutesEnum.NOT_FOUND_PAGE,
+        loadComponent: () => import('../../common/not-found-page/not-found-page.component').then((m) => m.NotFoundPageComponent)
+      },
+    ]
+  },
 ];
 
 @NgModule({
