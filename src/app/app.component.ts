f27672eeed712a980e33e09babd96ee5bcd29d0c
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-17 18:49:29 +0430

    refactor(app.component.[html,ts]): remove main-layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,13 @@
-import { Component, OnDestroy} from '@angular/core';
-import { Subscription } from "rxjs";
-
-import { MainLayoutService } from "./services/main-layout.service";
-import { HeaderModel } from "@shared-components/src/app/main-layout/models/header.model";
-import { ActionbarModel } from "@shared-components/src/app/main-layout/models/actionbar.model";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy{
+export class AppComponent{
   title = 'survey-front';
-  headerConfig: HeaderModel;
-  actionbarConfig: ActionbarModel;
 
-  subscriptions = new Subscription();
+  constructor() {}
 
-  constructor(private layoutService: MainLayoutService) {
-    this.subscriptions.add(
-      this.layoutService.headerConfigSubject.subscribe(header => {
-        this.headerConfig = header;
-      })
-    )
-    this.subscriptions.add(
-      this.layoutService.actionbarConfigSubject.subscribe(actionbar => {
-        this.actionbarConfig = actionbar;
-      })
-    )
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.unsubscribe();
-  }
 }
