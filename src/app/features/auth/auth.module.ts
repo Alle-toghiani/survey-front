d3c05e28496c55362ba015c1d333c0a7c4b1dcee
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-17 19:41:20 +0430

    feat(auth/login): create login template

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { TranslateModule } from "@ngx-translate/core";
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from "ng-zorro-antd/input";
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzCheckboxModule } from "ng-zorro-antd/checkbox";
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+
 
 
 @NgModule({
@@ -13,7 +22,15 @@ import { SignupComponent } from './components/signup/signup.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    TranslateModule,
+    NzInputModule,
+    NzFormModule,
+    NzButtonModule,
+    NzRadioModule,
+    NzCheckboxModule
   ]
 })
 export class AuthModule { }
--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  validateForm = new FormGroup({
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+    remember: new FormControl(true)
+  })
+
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  submitForm(): void {
+    console.log('submit', this.validateForm.value);
   }
 
 }
