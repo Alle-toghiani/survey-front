0cfade37ca43590d494fc37f6708541b00dc6ef9
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-19 18:53:09 +0430

    feat(auth/signup.component.*): wireup signup button, add password confirm, email fields

--- src/app/features/auth/components/signup/signup.component.ts
+++ src/app/features/auth/components/signup/signup.component.ts
@@ -1,26 +1,73 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+
+import {Subscription} from "rxjs";
+
+import {RoutesEnum} from "@enums";
+import {AuthHttpService} from "../../services/auth-http.service";
+import {SignupReqModel} from "../../models/signup-req.model";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
 
-  validateForm = new FormGroup({
+  isLoadingSubmitBtn= false;
+  subscriptions = new Subscription();
+
+  signupForm = new FormGroup({
     username: new FormControl(null, Validators.required),
-    password: new FormControl(null, Validators.required),
-    remember: new FormControl(true)
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl(null, [Validators.required, SignupComponent.matchValues('password')]),
   })
 
-  constructor() { }
+  constructor(
+    private authService: AuthHttpService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
-  submitForm(): void {
-    console.log('submit', this.validateForm.value);
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 
+  submitForm(): void {
+    this.isLoadingSubmitBtn = true;
+    const signupData: SignupReqModel = {
+      username: this.signupForm.value.username,
+      email: this.signupForm.value.email,
+      password: this.signupForm.value.password,
+    }
+    this.subscriptions.add(
+      this.authService.signup(signupData).subscribe(
+        {
+          next: signupRes => {
+            this.isLoadingSubmitBtn = false;
+            if (signupRes.success){
+              this.router.navigate(['/'+RoutesEnum.AUTH, RoutesEnum.LOGIN])
+            }
+          }
+        }
+      )
+    )
+  }
+
+
+  public static matchValues(
+    matchTo: string // name of the control to match to
+  ): (AbstractControl) => ValidationErrors | null {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return !!control.parent &&
+      !!control.parent.value &&
+      control.value === control.parent.controls[matchTo].value
+        ? null
+        : { isMatching: false };
+    };
+  }
 }
