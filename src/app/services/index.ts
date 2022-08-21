cfeff0ec4002a8eec03acd9b58328eac903d2fe5
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-21 20:59:17 +0430

    feat(services/index.ts): export RequestSetToken, TokenService

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -2,3 +2,5 @@ export { MainLayoutService } from './main-layout.service';
 export { CustomValidatorsService } from './custom-validators.service';
 export { SurveyDetailsResolver } from './survey-details.resolver';
 export { ResponseNotificationInterceptor } from './http-interceptors/response-notification.interceptor';
+export { RequestSetTokenInterceptor } from './http-interceptors/request-set-token.interceptor';
+export { TokenService } from './token.service';
