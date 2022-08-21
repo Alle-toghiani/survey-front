c027d420981401fd49cee80b11a39c1a6503ca4f
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-21 18:53:52 +0430

    feat(auth/login.component): wire up login button

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {AuthHttpService} from "../../services/auth-http.service";
+import {Router} from "@angular/router";
+import {RoutesEnum} from "@enums";
 
 @Component({
   selector: 'app-login',
@@ -8,19 +12,44 @@ import { FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class LoginComponent implements OnInit {
 
-  validateForm = new FormGroup({
+  isLoadingSubmitBtn= false;
+  subscriptions = new Subscription();
+
+  loginForm = new FormGroup({
     username: new FormControl(null, Validators.required),
     password: new FormControl(null, Validators.required),
     remember: new FormControl(true)
   })
 
-  constructor() {}
+  constructor(
+    private authService: AuthHttpService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
   }
 
   submitForm(): void {
-    console.log('submit', this.validateForm.value);
+    this.isLoadingSubmitBtn = true;
+    const loginData = {
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password
+    }
+    this.subscriptions.add(
+      this.authService.signIn(loginData).subscribe(
+        {
+          next: loginRes => {
+            this.isLoadingSubmitBtn = false;
+            if(loginRes.success){
+              this.router.navigate([RoutesEnum.DASHBOARD]);
+            }
+          },
+          error: err => {
+            this.isLoadingSubmitBtn = false;
+          }
+        }
+      )
+    )
   }
 
 }
