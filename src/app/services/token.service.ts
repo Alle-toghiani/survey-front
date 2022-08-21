b6c6acb0ab6f578300f4f089f53e75c32387a167
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-21 20:58:45 +0430

    feat(services): create token.service

---
+++ src/app/services/token.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from "@auth0/angular-jwt";
+import {Router} from "@angular/router";
+import {RoutesEnum} from "@enums";
+import {NzNotificationService} from "ng-zorro-antd/notification";
+import {TranslateService} from "@ngx-translate/core";
+import {Subscription} from "rxjs";
+
+const enum LocalStorageKeys {
+  CLIENT_TOKEN = 'client-token'
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  jwtService: JwtHelperService;
+  subscriptions = new Subscription();
+
+  constructor(
+    private router: Router,
+    private notificationService: NzNotificationService,
+    // private translateService: TranslateService
+  ) {
+    this.jwtService = new JwtHelperService();
+  }
+
+  setToken(token: string): boolean{
+    if (token){
+      try {
+        const decodedToken = this.jwtService.decodeToken(token);
+        console.log(decodedToken);
+        if (decodedToken.username){
+          localStorage.setItem(LocalStorageKeys.CLIENT_TOKEN, token);
+          return true;
+        }
+      } catch (e) {
+        return false;
+      }
+
+    }
+    return false;
+  }
+
+  getToken(): string{
+    const token = localStorage.getItem(LocalStorageKeys.CLIENT_TOKEN);
+    if (token){
+      if ( !this.jwtService.isTokenExpired(token)){
+        return token;
+      } else {
+        this.onTokenExpired();
+      }
+    } else {
+      this.onNavigateToLogin();
+    }
+    return undefined;
+  }
+
+  onTokenExpired(): void{
+    // FIXME: injecting translateService causes Circular dependency;
+    // this.notificationService.error(this.translator('auth.token-expired'), this.translator('auth.please-relogin'));
+    this.notificationService.error('Token Expired', 'Please re-Login');
+
+    this.onNavigateToLogin();
+  }
+
+  onNavigateToLogin(): void{
+    localStorage.clear();
+    this.router.navigate(['/'+ RoutesEnum.AUTH, RoutesEnum.LOGIN])
+  }
+
+  onSignOut(): void{
+    this.onNavigateToLogin();
+  }
+
+  // translator(word: string): string{
+  //   let translatedWord = "";
+  //   this.subscriptions.add(this.translateService.get(word.toLowerCase()).subscribe(
+  //     result => {
+  //       translatedWord = result;
+  //     }
+  //   ));
+  //   return translatedWord;
+  // }
+
+}
