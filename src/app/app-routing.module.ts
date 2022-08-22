295cc328582b09e553a7e6f073c6fd2cafa8caa7
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-22 18:51:26 +0430

    feat/style(app-routing.module.ts): import auth.guard and use it for Dashboard routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,32 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {RoutesEnum} from "@enums";
+import {AuthGuard} from "./guards/auth.guard";
 
 const routes: Routes = [
-  {path: '', redirectTo: RoutesEnum.DASHBOARD, pathMatch: 'full'},
-  {path: RoutesEnum.DASHBOARD, loadChildren: ()=> import('./features/dashboard/dashboard.module').then(m => m.DashboardModule)},
-  {path: RoutesEnum.AUTH, loadChildren: ()=> import('./features/auth/auth.module').then(m => m.AuthModule)},
-  {path: RoutesEnum.NOT_FOUND_PAGE, loadComponent: () => import('./common/not-found-page/not-found-page.component').then((m) => m.NotFoundPageComponent)},
-  {path: '**', redirectTo: RoutesEnum.NOT_FOUND_PAGE}
+  {
+    path: '',
+    redirectTo: RoutesEnum.DASHBOARD,
+    pathMatch: 'full'
+  },
+  {
+    path: RoutesEnum.DASHBOARD,
+    loadChildren: ()=> import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
+    canLoad: [AuthGuard],
+    canActivateChild: [AuthGuard]
+  },
+  {
+    path: RoutesEnum.AUTH,
+    loadChildren: ()=> import('./features/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: RoutesEnum.NOT_FOUND_PAGE,
+    loadComponent: () => import('./common/not-found-page/not-found-page.component').then((m) => m.NotFoundPageComponent)
+  },
+  {
+    path: '**',
+    redirectTo: RoutesEnum.NOT_FOUND_PAGE
+  }
 ];
 
 @NgModule({
