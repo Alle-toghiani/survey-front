15d3ecfa8b21ba7159db26cdf0cd5734d5a2f150
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-25 17:54:26 +0430

    feat(enums/routes.enum): add REPORT_PARAM and ADMINS

--- src/app/enums/routes.enum.ts
+++ src/app/enums/routes.enum.ts
@@ -3,12 +3,14 @@ export enum RoutesEnum {
   SURVEY_PREVIEW = 'preview',
   SURVEY_ID_PARAM = 'sid',
   QUESTION_ID_PARAM = 'qid',
+  REPORT_PARAM = 'reportCode',
 
   AUTH = 'auth',
   LOGIN = 'login',
   SIGNUP = 'signup',
 
   API_TOKEN = 'api-token',
+  ADMINS = 'manage-admins',
 
   NOT_FOUND_PAGE = 'not-found',
   DASHBOARD = 'dashboard'
