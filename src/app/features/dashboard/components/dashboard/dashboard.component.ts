6e378db375958ba910aff338e6ee40014a810d22
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-25 17:54:07 +0430

    feat(dashboard.component.ts): add apiToken and admins button to actionbar

--- src/app/features/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/components/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+
 import { Subscription } from "rxjs";
 
-import { DashboardHttpService } from "../../services/dashboard-http.service";
-import { FolderModel } from "@models";
-import {HeaderModel} from "@shared-components/src/app/main-layout/models/header.model";
-import {ActionbarModel} from "@shared-components/src/app/main-layout/models/actionbar.model";
-import {MainLayoutService} from "@services";
+import { HeaderModel} from "@shared-components/src/app/main-layout/models/header.model";
+import { ActionbarModel} from "@shared-components/src/app/main-layout/models/actionbar.model";
+
+import { MainLayoutService, TokenService} from "@services";
+import { RoutesEnum} from "@enums";
 
 @Component({
   selector: 'app-dashboard',
@@ -15,27 +17,50 @@ import {MainLayoutService} from "@services";
 export class DashboardComponent implements OnInit, OnDestroy {
 
   headerConfig: HeaderModel;
-  actionbarConfig: ActionbarModel;
+  actionbarConfig: ActionbarModel = {
+    isBackDisabled: true,
+    actions: [
+      {
+        title: 'dashboard.actionbar.manage-apiKey',
+        iconType: 'key',
+        color:'green',
+        link: ['/'+RoutesEnum.DASHBOARD, RoutesEnum.API_TOKEN]
+      },
+      {
+        title:'dashboard.actionbar.manage-mods',
+        iconType: 'crown',
+        color:'orange',
+        link: ['/'+RoutesEnum.DASHBOARD, RoutesEnum.ADMINS]
+      }
+    ]
+  }
 
   subscriptions = new Subscription();
 
-  constructor(private layoutService: MainLayoutService) {
-    this.subscriptions.add(
-      this.layoutService.headerConfigSubject.subscribe(header => {
-        this.headerConfig = header;
-      })
-    )
-    this.subscriptions.add(
-      this.layoutService.actionbarConfigSubject.subscribe(actionbar => {
-        this.actionbarConfig = actionbar;
-      })
-    )
+  constructor(
+    private layoutService: MainLayoutService,
+    private tokenService: TokenService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd && val.url === '/'+ RoutesEnum.DASHBOARD) {
+        this.initDashboardActionbar();
+      }
+    });
   }
 
   ngOnInit() {
+    this.initDashboardActionbar();
   }
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
+  }
+
+  private initDashboardActionbar(): void{
+    if (this.tokenService.isUserAdmin()){
+      this.layoutService.actionbarConfigSubject.next(this.actionbarConfig);
+    }
   }
 }
