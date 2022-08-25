abf80c69aafe5be23cbb8fd26ca11f2c615cc429
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-25 15:55:34 +0430

    feat(app/services/survey-http.service): add getSurvey, getSurveyQuestion updateChartSettings methods

--- src/app/services/survey-http.service.ts
+++ src/app/services/survey-http.service.ts
@@ -4,7 +4,7 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {EMPTY, Observable} from "rxjs";
 
 import {environment} from "@environments/environment";
-import {SurveyModel} from "@models";
+import {CustomChartSettings, SharedModel, SurveyModel, SurveyQuestion} from "@models";
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +18,18 @@ export class SurveyHttpService {
   getSurveyReportData(reportId: string):Observable<any>{
     const url = environment.backendBaseUrl + 'survey/r/' + reportId;
     return this.http.get(url);
+  }
+
+  getSurvey(surveyId: number): Observable<SharedModel<SurveyModel>>{
+    return this.http.get<SharedModel<SurveyModel>>(environment.backendBaseUrl + 'survey/' + surveyId);
+  }
+
+  getSurveyQuestionDetails(sid: string, qid: string): Observable<SharedModel<SurveyQuestion>>{
+    return this.http.get<SharedModel<SurveyQuestion>>(environment.backendBaseUrl + 'survey/' + sid + '/details/' + qid)
+  }
+
+  updateChartSettings(sid: string, qid: string, data: CustomChartSettings): Observable<SharedModel<any>>{
+    const url = environment.backendBaseUrl + 'survey/' + sid + '/details/' + qid
+    return this.http.put<SharedModel<any>>(url, data);
   }
 }
