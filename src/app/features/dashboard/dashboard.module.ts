4200ef988de8f259897d1510e47b6c306592f97a
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-27 19:09:11 +0430

    feat(dashboard.module): import Nz[Modal, Form, Alert]Module, import FormModule ReactiveFormsModule

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { TranslateModule } from "@ngx-translate/core";
+import { NzFormModule } from "ng-zorro-antd/form";
 import { NzCollapseModule } from "ng-zorro-antd/collapse";
 import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzModalModule } from "ng-zorro-antd/modal";
+import { NzAlertModule } from "ng-zorro-antd/alert";
 
 import { MainLayoutModule} from "@shared-components/src/app/main-layout/main-layout.module";
 import { WidgetContainerModule } from "@shared-components/src/app/widget-container/widget-container.module";
@@ -12,6 +16,7 @@ import { TablesModule } from "@shared-components/src/app/tables/tables.module";
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { FoldersSurveysListComponent } from './components/folders-surveys-list/folders-surveys-list.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 
 
 
@@ -28,7 +33,12 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     NzCollapseModule,
     NzButtonModule,
     TablesModule,
-    MainLayoutModule
+    MainLayoutModule,
+    NzModalModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzAlertModule
   ]
 })
 export class DashboardModule { }
