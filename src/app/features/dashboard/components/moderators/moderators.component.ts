c49bcf84d8807a09d241261dab765d1ccb9cdce3
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-27 16:54:16 +0430

    feat(modetators.component): wire up create admin, show alerts after creation

--- src/app/features/dashboard/components/moderators/moderators.component.ts
+++ src/app/features/dashboard/components/moderators/moderators.component.ts
@@ -6,9 +6,13 @@ import {TranslateModule} from "@ngx-translate/core";
 import {NzFormModule} from "ng-zorro-antd/form";
 import {NzButtonModule} from "ng-zorro-antd/button";
 import {NzDividerModule} from "ng-zorro-antd/divider";
+import {NzAlertModule} from "ng-zorro-antd/alert";
 
 import {WidgetContainerModule} from "@shared-components/src/app/widget-container/widget-container.module";
 import {Subscription} from "rxjs";
+import {DashboardHttpService} from "../../services/dashboard-http.service";
+import {RoutesEnum} from "@enums";
+import {ClipboardService} from "ngx-clipboard";
 
 @Component({
   selector: 'app-moderators',
@@ -22,31 +26,80 @@ import {Subscription} from "rxjs";
     ReactiveFormsModule,
     NzFormModule,
     NzButtonModule,
-    NzDividerModule
+    NzDividerModule,
+    NzAlertModule
   ],
   styleUrls: ['./moderators.component.scss']
 })
 export class ModeratorsComponent implements OnInit {
 
-  isLoadingWidget = false;
-
-  isLoadingSubmitBtn = false;
-
-  createdModLink: string;
-
+  subscriptions = new Subscription();
   createModForm = new FormGroup({
     username: new FormControl(null, Validators.required)
   })
 
-  subscriptions = new Subscription();
+  isLoadingWidget = false;
+  isLoadingSubmitBtn = false;
+  createdUser: {username: string, password: string, link: string}
+  copyBtnText = 'common.copy';
 
-  constructor() { }
+  modsList: {username:string, surveys: string[]}[] = [];
+
+  constructor(
+    private dashboardHttpService: DashboardHttpService,
+    private clipboardService: ClipboardService
+  ) { }
 
   ngOnInit(): void {
+    this.getModsList();
   }
 
   submitForm(): void{
+    const modUsername = this.createModForm.value.username;
+    this.isLoadingSubmitBtn = true;
+    if (modUsername){
+      this.subscriptions.add(
+        this.dashboardHttpService.createMod(modUsername).subscribe(
+          {
+            next: (res) => {
+              this.isLoadingSubmitBtn = false;
+              if (res.success){
+                this.createdUser = {...res.data, link: this.generateLink(res.data)};
+                this.getModsList();
+              }
+            },
+            error: err => {
+              this.isLoadingSubmitBtn = false
+            }
+          }
+        )
+      )
+    }
+  }
 
+  getModsList(): void{
+    this.subscriptions.add(
+      this.dashboardHttpService.getModsList().subscribe(
+        {
+          next: (modsListRes) => {
+            if (modsListRes.success){
+              this.modsList = modsListRes.data;
+            }
+          }
+        }
+      )
+    )
+  }
+
+  generateLink(user){
+    if (user.username && user.password){
+      return window.origin + '/' + RoutesEnum.AUTH + '/' + RoutesEnum.LOGIN + '?username=' + user.username + '&password=' + user.password;
+    } else return '';
+  }
+
+  onCopyLink(){
+    this.clipboardService.copy(this.createdUser.link);
+    this.copyBtnText = 'common.copied';
   }
 
 }
