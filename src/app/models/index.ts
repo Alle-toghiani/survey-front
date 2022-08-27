27f649e54d389ba146a5ccaeb7261eed3f929f6e
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-27 11:54:27 +0430

    feat(models/index.ts): export PorslineSurvey, PorslineQuestion, PorslineReport, SurveyReport

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,3 +1,6 @@
 export { SharedModel } from './shared.model';
 export { FolderModel } from './folder.model';
 export * from './survey.model';
+export { PorslineSurvey } from './porsline-models/porsline-survey.model';
+export { PorslineQuestion } from './porsline-models/porsline-question.model';
+export { PorslineReport, SurveyReport } from './porsline-models/porsline-reports.model';
