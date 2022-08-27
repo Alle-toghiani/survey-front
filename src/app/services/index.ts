938eab216282d5bc4c90430d65303268919c7e51
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-27 11:58:37 +0430

    feat(services/index.ts): export survey-http.service

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -4,3 +4,4 @@ export { SurveyDetailsResolver } from './survey-details.resolver';
 export { ResponseNotificationInterceptor } from './http-interceptors/response-notification.interceptor';
 export { RequestSetTokenInterceptor } from './http-interceptors/request-set-token.interceptor';
 export { TokenService } from './token.service';
+export { SurveyHttpService } from './survey-http.service';
