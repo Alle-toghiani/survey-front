31fb661b378ae9ef2a9f789bfbe3bc040a51f2a1
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-27 19:09:53 +0430

    feat(folders-surveys-list): implement initialize survey modal

--- src/app/features/dashboard/components/folders-surveys-list/folders-surveys-list.component.ts
+++ src/app/features/dashboard/components/folders-surveys-list/folders-surveys-list.component.ts
@@ -5,6 +5,7 @@ import { ClipboardService } from 'ngx-clipboard'
 
 import { FolderModel } from "@models";
 import { DashboardHttpService } from "../../services/dashboard-http.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-folders-surveys-list',
@@ -14,8 +15,16 @@ import { DashboardHttpService } from "../../services/dashboard-http.service";
 export class FoldersSurveysListComponent implements OnInit {
 
   subscriptions = new Subscription();
+  createSurveyForm = new FormGroup({
+    name: new FormControl(null,Validators.required)
+  })
   folders: FolderModel[] = [];
+  selectedFolderId: number;
   isLoading = true;
+
+  isVisibleModal = false;
+  isLoadingModalSubmitBtn = false;
+  surveyCreatedSuccessfully = false;
 
   copiedReportCode: string;
 
@@ -46,8 +55,52 @@ export class FoldersSurveysListComponent implements OnInit {
   }
 
   onCopySurveyReportLink(reportCode: string): void{
-    const url = 'https://survey.porsline.ir/r/' + reportCode;
+    const url = window.origin + '/r/' + reportCode;
     this.clipboardService.copy(url);
     this.copiedReportCode = reportCode;
+  }
+
+  onShowCreateModal(folderId, event): void{
+    event.stopPropagation();
+    this.selectedFolderId = folderId;
+    this.isVisibleModal = true;
+  }
+
+  onModalCancel(){
+    this.surveyCreatedSuccessfully = false;
+    this.selectedFolderId = undefined;
+    this.isVisibleModal = false;
+    this.createSurveyForm.reset();
+  }
+
+  onModalSubmit(){
+    const newSurveyTitle = this.createSurveyForm.value.name;
+    if (newSurveyTitle && this.selectedFolderId){
+      const createSurveyData = {
+        folder : this.selectedFolderId,
+        name: newSurveyTitle
+      }
+      this.isLoadingModalSubmitBtn = true;
+      this.subscriptions.add(
+        this.dashboardService.initializeSurvey(createSurveyData).subscribe(
+          {
+            next: initRes => {
+              this.isLoadingModalSubmitBtn = false;
+              if (initRes.success){
+                this.getFoldersData();
+                this.surveyCreatedSuccessfully = true;
+                setTimeout( () => {
+                  this.onModalCancel();
+                }, 3000);
+
+              }
+            },
+            error: err => {
+              this.isLoadingModalSubmitBtn = false;
+            }
+          }
+        )
+      )
+    }
   }
 }
