3a1257d6b8fc98513e9cd30b9519519b75349660
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-27 11:53:29 +0430

    feat/refactor(results-container.component): add support to show reports page charts

--- src/app/features/results/components/results-container/results-container.component.ts
+++ src/app/features/results/components/results-container/results-container.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
-import { SharedModel, SurveyModel } from "@models";
+import { MainLayoutService } from "@services";
+import { PorslineQuestion, SharedModel, SurveyReport } from "@models";
+import { RoutesEnum } from "@enums";
+import { Question } from "../../../../models/porsline-models/porsline-reports.model";
+import { SurveyHttpService } from "../../../../services/survey-http.service";
 
 @Component({
   selector: 'app-results-container',
@@ -10,13 +14,44 @@ import { SharedModel, SurveyModel } from "@models";
 })
 export class ResultsContainerComponent implements OnInit {
 
-  survey: SurveyModel;
+  questionsArray: Question[] | PorslineQuestion[];
+  singleQuestion: PorslineQuestion;
+
+  isListMode = true;
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
+    private mainLayoutService: MainLayoutService,
+    private surveyHttpService: SurveyHttpService
   ) { }
 
   ngOnInit(): void {
-    this.survey = (this.route.snapshot.data['surveyResolverData'] as SharedModel<SurveyModel>).data;
+    const sid = this.route.snapshot.params[RoutesEnum.SURVEY_ID_PARAM];
+    if (sid){
+      this.surveyHttpService.getPreview(sid).subscribe(
+        res => {
+          if (res.success){
+            this.questionsArray = res.data;
+          }
+        }
+      )
+    }
+
+    if (this.route.snapshot.params[RoutesEnum.REPORT_PARAM]){
+      this.isListMode = false;
+      this.questionsArray = (this.route.snapshot.data['surveyResolverData'] as SharedModel<SurveyReport>).data.questions;
+      this.mainLayoutService.actionbarConfigSubject.next(
+        {
+          isBackDisabled: true,
+          actions: [{
+            title: 'results.actionbar.users-login',
+            iconType: 'user',
+            color: 'orange',
+            link: ['/' + RoutesEnum.AUTH, RoutesEnum.LOGIN]
+          }]
+        }
+      )
+    }
   }
 }
