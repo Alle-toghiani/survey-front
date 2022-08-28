dea6be5138f3ef5a7e825a36ea48e319a66bc786
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-28 07:27:54 +0430

    feat(manage-reports): declare WelcomeTabComponent, MultipleQuestionTabComponent

--- src/app/features/manage-reports/manage-reports.module.ts
+++ src/app/features/manage-reports/manage-reports.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { TranslateModule} from "@ngx-translate/core";
+import { TranslateModule } from "@ngx-translate/core";
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTabsModule } from "ng-zorro-antd/tabs";
+import { NzFormModule } from "ng-zorro-antd/form";
+import { NzIconModule } from "ng-zorro-antd/icon";
 
 import { WidgetContainerModule} from "@shared-components/src/app/widget-container/widget-container.module";
 import { TablesModule} from "@shared-components/src/app/tables/tables.module";
 import { ManageReportsRoutingModule } from './manage-reports-routing.module';
 import { ReportsListComponent } from './components/reports-list/reports-list.component';
+import { WelcomeTabComponent } from './components/welcome-tab/welcome-tab.component';
+import {NzAlertModule} from "ng-zorro-antd/alert";
+import { MultipleQuestionTabComponent } from './components/multiple-question-tab/multiple-question-tab.component';
+
+
 
 @NgModule({
   declarations: [
-    ReportsListComponent
+    ReportsListComponent,
+    WelcomeTabComponent,
+    MultipleQuestionTabComponent
   ],
   imports: [
     CommonModule,
@@ -19,7 +30,13 @@ import { ReportsListComponent } from './components/reports-list/reports-list.com
     WidgetContainerModule,
     TablesModule,
     NzButtonModule,
-    TranslateModule
+    TranslateModule,
+    NzTabsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzAlertModule,
+    NzIconModule
   ]
 })
 export class ManageReportsModule { }
